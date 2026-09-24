1473329a23f6f6e767ab454c34cab90e3e4a8ec4
Dâmaso Constantino <damaso.constantino@protonmail.com>
2026-09-24 23:32:48 +0200

    test(adapter-utils): wait for proxy streams to close

--- packages/adapter-utils/src/execution-target-sandbox.test.ts
+++ packages/adapter-utils/src/execution-target-sandbox.test.ts
@@ -128,7 +128,7 @@ describe("sandbox adapter execution targets", () => {
         clearTimeout(timeout);
         reject(error);
       });
-      child.on("exit", (exitCode) => {
+      child.on("close", (exitCode) => {
         clearTimeout(timeout);
         resolve(exitCode);
       });
